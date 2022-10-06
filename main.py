2229bd3ecd6418d3d6c8ea5f1099b22f3923389f
Armin <arminonline71@gmail.com>
2022-10-06 19:35:08 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+from PIL import Image
+import json
+import torchvision
+import torch
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision import transforms as T
+from detection.engine import evaluate, train_one_epoch
+from detection import utils
+
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+  
+  
